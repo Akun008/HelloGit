81ed5d3d3c39aadbd33223989b309c629ec2544b
张乔坤 <2523535763@qq.com>
2022-02-17 18:11:50 +0800

    创建分支

--- HelloGit/src/main/java/com/itcast/git/HelloGit.java
+++ HelloGit/src/main/java/com/itcast/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.printf("Hello Git");
         System.out.printf("更新1");
+        System.out.printf("创建分支");
     }
 }
